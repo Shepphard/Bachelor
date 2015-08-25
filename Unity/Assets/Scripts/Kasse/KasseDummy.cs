5822451953b68b1c74a12bb03113c5e85b6754b0
Philippe Kayser <philippekayser@gmail.com>
2015-08-25 16:42:00 +0200

    Committing first try

---
+++ Unity/Assets/Scripts/Kasse/KasseDummy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KasseDummy : MonoBehaviour {
+
+	private Dictionary<Article, int> articles;
+	private int tableID;
+
+	public void Start()
+	{
+		articles = new ArrayList ();
+	}
+
+	public void addArticle(int id, float preis)
+	{
+		articles.Add (new Article (id, preis));
+	}
+}

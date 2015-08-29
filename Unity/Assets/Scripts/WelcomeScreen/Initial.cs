d08e65a30990a320a391fd661dd6705c71fbf5c0
Philippe Kayser <philippekayser@gmail.com>
2015-08-29 09:48:51 +0200

    Commit

---
+++ Unity/Assets/Scripts/WelcomeScreen/Initial.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Text;
+
+public class Initial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		WebClient client = new WebClient();
+		string url = "http://localhost/Server/init.php?type=abs";
+		byte[] html = client.DownloadData (url);
+		UTF8Encoding utf = new UTF8Encoding ();
+		string mystring = utf.GetString (html);
+
+		Debug.Log (mystring);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
